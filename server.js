7939df0a62b49ae31d3b2a06c66b3137c8d2ec0b
rm3572529-svg <rm3572529@gmail.com>
2025-12-04 20:11:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,38 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const path = require("path");
-const fs = require("fs");
 
 app.use(cors());
 app.use(express.json());
 
-// Load keys.json
-const keysPath = path.join(__dirname, "keys.json");
-let keys = [];
-
-if (fs.existsSync(keysPath)) {
-  keys = JSON.parse(fs.readFileSync(keysPath, "utf8"));
-}
-
-// API to generate key
-app.get("/generate", (req, res) => {
-  const newKey = "RAJ-" + Math.random().toString(36).substring(2, 10).toUpperCase();
-  keys.push(newKey);
-
-  fs.writeFileSync(keysPath, JSON.stringify(keys, null, 2));
-
-  res.json({ key: newKey });
+// Test route
+app.get("/", (req, res) => {
+  res.send("✅ Raj Keygen Server Working!");
 });
 
-// Serve frontend
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-// Start server
+// Render uses process.env.PORT
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
